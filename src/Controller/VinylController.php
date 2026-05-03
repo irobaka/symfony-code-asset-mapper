8ad300dbeee7ac4bf1a772974b35c3c8a2bf5c46
Grzegorz Majta <grzegorz.majta@gmail.com>
2026-05-03 07:38:24 +0200

    update to symfony 6.4

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -8,7 +8,7 @@ use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
@@ -37,7 +37,7 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(VinylMixRepository $mixRepository, Request $request, string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, Request $request, ?string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
