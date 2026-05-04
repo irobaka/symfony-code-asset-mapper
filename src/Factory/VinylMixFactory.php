8572a5c7e978185a9e4260bdd2c9eb0fc7566799
Grzegorz Majta <grzegorz.majta@gmail.com>
2026-05-04 07:30:32 +0200

    update

--- src/Factory/VinylMixFactory.php
+++ src/Factory/VinylMixFactory.php
@@ -3,39 +3,16 @@
 namespace App\Factory;
 
 use App\Entity\VinylMix;
-use App\Repository\VinylMixRepository;
-use Zenstruck\Foundry\RepositoryProxy;
-use Zenstruck\Foundry\ModelFactory;
-use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 
-/**
- * @extends ModelFactory<VinylMix>
- *
- * @method static VinylMix|Proxy createOne(array $attributes = [])
- * @method static VinylMix[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static VinylMix|Proxy find(object|array|mixed $criteria)
- * @method static VinylMix|Proxy findOrCreate(array $attributes)
- * @method static VinylMix|Proxy first(string $sortedField = 'id')
- * @method static VinylMix|Proxy last(string $sortedField = 'id')
- * @method static VinylMix|Proxy random(array $attributes = [])
- * @method static VinylMix|Proxy randomOrCreate(array $attributes = [])
- * @method static VinylMix[]|Proxy[] all()
- * @method static VinylMix[]|Proxy[] findBy(array $attributes)
- * @method static VinylMix[]|Proxy[] randomSet(int $number, array $attributes = [])
- * @method static VinylMix[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
- * @method static VinylMixRepository|RepositoryProxy repository()
- * @method VinylMix|Proxy create(array|callable $attributes = [])
- */
-final class VinylMixFactory extends ModelFactory
+final class VinylMixFactory extends PersistentProxyObjectFactory
 {
-    public function __construct()
+    public static function class(): string
     {
-        parent::__construct();
-
-        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+        return VinylMix::class;
     }
 
-    protected function getDefaults(): array
+    protected function defaults(): array|callable
     {
         return [
             'title' => self::faker()->words(5, true),
@@ -44,18 +21,5 @@ final class VinylMixFactory extends ModelFactory
             'genre' => self::faker()->randomElement(['pop', 'rock']),
             'votes' => self::faker()->numberBetween(-50, 50),
         ];
-    }
-
-    protected function initialize(): self
-    {
-        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-        return $this
-            // ->afterInstantiate(function(VinylMix $vinylMix): void {})
-        ;
-    }
-
-    protected static function getClass(): string
-    {
-        return VinylMix::class;
     }
 }
